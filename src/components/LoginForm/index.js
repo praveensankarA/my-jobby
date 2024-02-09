31352d37d82b98d7659a19d2e1b4181d869bb4c4
praveensankarA <praveenedu999@gmail.com>
2024-02-09 15:21:04 +0000

    first commit

---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,15 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+      </div>
+    )
+  }
+}
+
+export default LoginForm
